82d0f3b0a488760c5f32b6809bc50ba57104431b
nikhilsingh1999 <brajendrarajpoot80@gmail.com>
2025-06-08 15:59:44 +0530

    fix bugs and add distributor button2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,6 @@ import Link from "next/link"
 import Image from "next/image"
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
-import { ThemeToggle } from "@/components/ui/theme-toggle.tsx/theme-toggle"
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
